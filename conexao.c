56128cee23510c05e01a2d601dc4f8c33d868133
mariana <marianatassan19@gmail.com>
2023-04-27 16:23:42 -0300

    escopo funções de conexão

---
+++ conexao.c
@@ -0,0 +1,17 @@
+// cria a conexão com o sensor e retorna um ponteiro NULL em caso de falha
+// *chIP: endereço IP do sensor
+// *chPort: numero da porta do sensor
+void* EthernetScanner_Connect(char *chIP, char *chPort, int iTimeOut)
+
+// verificar o status de conexão
+void EthernetScanner_GetConnectStatus(void *pEthernetScanner, int *uiConnectStatus)
+
+
+// Cria uma conexão UDP com o weCat3D
+// *chDestIP: endereço IP do sensor
+// *chDestPort: numero da porta do sensor
+// *chSrcIP: endereço IP da placa de interface de rede à qual o sensor está conectado
+void* EthernetScanner_ConnectUDP(char* chDestIP, char* chDestPort, char* chSrcIP, char* chSrcPort, char* chMode)
+
+// fecha a conexão
+void* _stdcall EthernetScanner_Disconnect(void *pEthernetScanner)
